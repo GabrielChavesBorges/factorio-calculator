66e78da85c5a538ac1454c434f5e047690be3c65
GabrielChavesBorges <51249944+GabrielChavesBorges@users.noreply.github.com>
2026-04-19 10:49:44 +0900

    add partial calculation structure

--- src/fctr.c
+++ src/fctr.c
@@ -31,3 +31,36 @@ void get_string_input(char** string_storage) {
 
     return;
 }
+
+void print_calculation(Item* target_item, size_t items_per_second) {
+    // final item calculation: items_per_second / batch_size;
+    // total_required_qty_per_second = final_item_factories * requirement_qty
+    // total_produced_per_second_per_machine = batch_size / process_time
+    // requirement_machine_qty: total_required_qty_per_second / total_produced_per_second_per_machine
+
+    return;
+}
+
+void calculation(Item* current_item, float required_qty, Order* order_tree) {
+    if (parent_item == NULL) {
+        return;
+    }
+
+    // calculate machine qty based on parent item
+    float qty_per_machine = current_item->batch_size * current_item->process_time_seconds;
+    float machine_qty = required_qty / qty_per_machine;
+    
+    // Tree tip condition
+    if (current_item->requirement_count = 0) {
+        return;
+    }
+
+    // call calculation on each requirement
+    for (size_t i = 0; i < current_item->requirement_count; i++) {
+        calculation(current_item->requirements[i]->item, machine_qty * current_item->requirements[i]->quantity);
+    }
+    
+    return;
+
+    // TODO: Figure how to store information in order tree so that it can be printed later
+}
--- src/fctr.h
+++ src/fctr.h
@@ -6,14 +6,14 @@
 typedef struct Item Item;
 
 typedef struct {
-    Item *item;
+    Item* item;
     size_t quantity;
 } Requirement;
 
 typedef struct Item {
-    char *name;
+    char* name;
     int batch_size;
-    float process_time_s;
+    float process_time_seconds;
     size_t requirement_count;
     Requirement *requirements;
 } Item;
@@ -23,8 +23,14 @@ typedef struct {
     size_t item_count;
 } Database;
 
+typedef struct {
+    char* item_name;
+    float machine_qty;
+} Order;
+
 
 Item* find_item(Database db, const char* item_name);
 void get_string_input(char** string_storage);
+void print_calculation(Item* target_item, size_t items_per_second);
 
 #endif
