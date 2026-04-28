5e07b9d393186bc509358f5678741aeef51d468c
Gabriel Borges <gabriel.chaves.borges@gmail.com>
2026-04-28 14:29:53 +0900

    add machine speed factor

--- src/data.c
+++ src/data.c
@@ -1,18 +1,18 @@
 #include "fctr.h"
 
-// Primary Items            Name              Batch|Time|Requirements
-Item coal               = {"coal",               1,  1.0, 0, NULL};
-Item copper_ore         = {"copper ore",         1,  1.0, 0, NULL};
-Item iron_ore           = {"iron ore",           1,  1.0, 0, NULL};
-Item stone              = {"stone",              1,  1.0, 0, NULL};
-Item water              = {"water",              1,  1.0, 0, NULL};
+// Primary Items          | Name             | Batch | Time                      | Requirements
+Item coal               = {"coal"            ,      1,  1.0 / DRILL_SPEED        , 0, NULL};
+Item copper_ore         = {"copper ore"      ,      1,  1.0 / DRILL_SPEED        , 0, NULL};
+Item iron_ore           = {"iron ore"        ,      1,  1.0 / DRILL_SPEED        , 0, NULL};
+Item stone              = {"stone"           ,      1,  1.0 / DRILL_SPEED        , 0, NULL};
+Item water              = {"water"           ,      1,  1.0 / OFFSHORE_PUMP_SPEED, 0, NULL};
 // Derived Items
-Item stone_brick        = {"stone brick",        1,  3.2, 1, (Requirement[]) {{ &stone, 2 }}};
-Item iron_plate         = {"iron plate",         1,  3.2, 1, (Requirement[]) {{ &iron_ore, 1 }}};
-Item iron_stick         = {"iron stick",         2,  0.5, 1, (Requirement[]) {{ &iron_plate, 1 }}};
-Item steel_plate        = {"steel plate",        1, 16.0, 1, (Requirement[]) {{ &iron_plate, 5 }}};
-Item concrete           = {"concrete",          10, 10.0, 3, (Requirement[]) {{ &iron_ore, 1 }, { &stone_brick, 5}, { &water, 100 }}};
-Item refined_concrete   = {"refined concrete",  10, 15.0, 4, (Requirement[]) {{ &concrete, 20 }, { &iron_stick, 8 }, { &steel_plate, 1 }, { &water, 100 }}};
+Item stone_brick        = {"stone brick"     ,      1,  3.2 / FURNACE_SPEED      , 1, (Requirement[]) {{ &stone, 2 }}};
+Item iron_plate         = {"iron plate"      ,      1,  3.2 / FURNACE_SPEED      , 1, (Requirement[]) {{ &iron_ore, 1 }}};
+Item steel_plate        = {"steel plate"     ,      1, 16.0 / FURNACE_SPEED      , 1, (Requirement[]) {{ &iron_plate, 5 }}};
+Item iron_stick         = {"iron stick"      ,      2,  0.5 / ASSEMBLY_SPEED     , 1, (Requirement[]) {{ &iron_plate, 1 }}};
+Item concrete           = {"concrete"        ,     10, 10.0 / ASSEMBLY_SPEED     , 3, (Requirement[]) {{ &iron_ore, 1 }, { &stone_brick, 5}, { &water, 100 }}};
+Item refined_concrete   = {"refined concrete",     10, 15.0 / ASSEMBLY_SPEED     , 4, (Requirement[]) {{ &concrete, 20 }, { &iron_stick, 8 }, { &steel_plate, 1 }, { &water, 100 }}};
 
 Item* all_items[] = {
     &coal,
--- src/fctr.h
+++ src/fctr.h
@@ -3,10 +3,24 @@
 
 #include <stddef.h>
 
-#define FACTORY_SPEED 1.0
-#define FURNACE_SPEED 1.0
-#define DRILL_SPEED   1.0
-#define PUMP_SPEED    1.0
+#define BURNER_MINING_DRILL_SPEED 0.25
+#define ELECTRIC_MINING_DRILL_SPEED 0.5
+#define STONE_FURNACE_SPEED 1.0
+#define STEEL_FURNACE_SPEED 2.0
+#define ASSEMBLING_MACHINE_1_SPEED 0.5
+#define ASSEMBLING_MACHINE_2_SPEED 0.75
+#define ASSEMBLING_MACHINE_3_SPEED 1.25
+
+#define OFFSHORE_PUMP_SPEED 1200.0
+#define PUMPJACK_SPEED 1.0
+
+#define DRILL_SPEED ELECTRIC_MINING_DRILL_SPEED
+#define FURNACE_SPEED STEEL_FURNACE_SPEED
+#define ASSEMBLY_SPEED ASSEMBLING_MACHINE_2_SPEED
+
+// 0.5
+// units/sec = mining_speed / mining_time
+// seconds for 1 unit = mining_time / mining_speed
 
 typedef struct Order Order;
 typedef struct Item Item;
